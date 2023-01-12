e119178c456dbcc867def3808d731fe53663e1b4
TouchMediaNetwork <cinmatick@ictyepprojects.com>
2023-01-12 10:33:07 +0100

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\RegisterController;
+
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Public routes
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::post('/logout',  [AuthController::class,  'logout'])->middleware('auth:sanctum');
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user(); 
 });
 
-Route::post('register', RegisterController::class);
-Route::post('login', RegisterController::class);
+
+
+
