414f1fcaa37a05693b94b08897de51c3ed6168e2
Emeks <emekaicthub@gmail.com>
2023-01-17 15:05:51 +0100

    landing page

--- resources/views/landingPage/download.blade.php
+++ resources/views/landingPage/download.blade.php
@@ -1,21 +1,22 @@
 <x-userLayout>
-   <div class="container text-center">
-    <a class="navbar-brand" href="/home">
-        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+   <div class="container text-center mt-5">
+    <a class="w-40 h-40 fill-current" href="#">
+        <img src="{{asset('assets/logo.png')}}" width="300" height="auto" class=" mx-auto d-block" alt="logo">
+
     </a>
         <div class="row p-5 bg-dark" >
 
-          <div class="col-12 col-md-6 text-center  p-5">
-            <div class="card text-center">
-                <a href="#" class="w-20 h-20 fill-current">
+          <div class="col-12 col-md-6 text-center bg-dark p-5">
+            <div class="  text-center">
+                <a href="#" class="w-20 h-20 ">
                     <img src="{{asset('assets/GooglePlay.png')}}" width="300" height="auto" class=" mx-auto d-block" alt="Play Store">
                 </a>
 
             </div>
           </div>
-          <div class="col-12 col-md-6 text-center  p-5">
-            <div class="card text-center">
-                <a href="#" class="w-20 h-20 fill-current">
+          <div class="col-12 col-md-6 text-center bg-dark p-5">
+            <div class=" text-center ">
+                <a href="#" class="w-20 h-20 ">
                     <img src="{{asset('assets/Apple store.png')}}"width="300" height="auto" class="mx-auto d-block" alt="Apple Store">
                 </a>
 
