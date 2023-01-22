91f93e3d8ec7cc4f986f4b37c8e3fccfbb3dabf2
Xpressglobe <tobeodiachi@yahoo.com>
2023-01-22 23:39:34 +0100

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Movie;
+use App\Models\Shows;
+use App\Models\Theatre;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +21,8 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(1)->create();
         Movie::factory(5)->create();
+        Shows::factory(5)->create();
+        Theatre::factory(5)->create();
         
 
 
